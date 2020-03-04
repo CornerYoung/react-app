5f951afc97d6cf479749a5983c4f19f5d89672ff
CornerYoung <947498533@qq.com>
2020-03-04 17:55:28 +0800

    安装antd组件库、配置babel-plugin-import（实现antd按需加载）

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Button} from 'antd';
+import { MessageOutlined } from '@ant-design/icons';
 
 function App() {
   return (
     <div>
       hello
+      <Button type="danger">按钮</Button>
+      <MessageOutlined style={{ fontSize: '16px', color: '#08c' }} />
     </div>
   );
 }
