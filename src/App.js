c4f9c0e90f5f4b5a5be240f1d7c51d63f9fbc14a
CornerYoung <947498533@qq.com>
2020-03-05 20:38:30 +0800

    HashRouter:使用锚点链接的形式|BrowserRouter:使用H5的新特性 history.push 如果上线之后，需要后台做一些重定向处理，否则进入某个页面刷新之后会出现404的bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import {Button} from 'antd';
-import { MessageOutlined } from '@ant-design/icons';
+import React from 'react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Mine from './pages/Mine'
+import Home from './pages/Home'
 
 function App() {
+  /**
+   * HashRouter:使用锚点链接的形式
+   * BrowserRouter:使用H5的新特性 history.push 如果上线之后，需要后台做一些重定向处理，否则进入某个页面刷新之后会出现404的bug
+  */
   return (
-    <div>
-      hello
-      <Button type="danger">按钮</Button>
-      <MessageOutlined style={{ fontSize: '16px', color: '#08c' }} />
-    </div>
+    <Router>
+      <Route path="/home" component={Home}></Route>
+      <Route path="/mine" component={Mine}></Route>
+    </Router>
   );
 }
 
