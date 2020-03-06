c39869bb83f2d259e36c2a92206da68ee5d0411a
CornerYoung <947498533@qq.com>
2020-03-06 20:33:57 +0800

    重定向push，repalce方法

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function NotFound() {
+export default function NotFound(props) {
+    const goBackHome = ()=>{
+        //push是叠加 replace是替换
+        //props.history.push('/')
+        props.history.replace('/')
+    }
     return (
         <div>
             页面被意大利炮轰走了···
+            <button onClick={goBackHome}>返回</button>
         </div>
     )
 }
