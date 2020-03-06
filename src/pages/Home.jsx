619648884faebc77263f2993860514fe49e760b5
CornerYoung <947498533@qq.com>
2020-03-06 16:26:10 +0800

    URLSearchParams和querystring读取参数

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import querystring from 'querystring'
 
-export default function Home() {
+export default function Home(props) {
+    const params = new URLSearchParams(props.location.search)
+    console.log(params.get('user'), params.get('age'))
+    const value = querystring.parse(props.location.search)
+    console.log(value)
     return (
         <div>
             Home
