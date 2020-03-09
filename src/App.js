e1a27e5927b2139a8db1ee0505741e59577fd80a
CornerYoung <947498533@qq.com>
2020-03-09 12:40:33 +0800

    使用react-redux进行状态管理

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import { increment, decrement } from './actions/counter'
 
 class App extends Component {
   render() {
+    console.log(this.props);
+    const { increment,decrement} =this.props
     return (
       <div className="container">
-        <h1 className="jumbotron-heading text-center">{this.props.value}</h1>
+        <h1 className="jumbotron-heading text-center">{this.props.counter}</h1>
         <p className="text-center">
-          <button onClick={this.props.onIncrement} className="btn btn-info">increment</button>
-          <button onClick={this.props.onDecrement} className="btn btn-success">decrement</button>
+          <button onClick={() => { increment() }} className="btn btn-info">increment</button>
+          <button onClick={() => { decrement() }} className="btn btn-success">decrement</button>
         </p>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    counter:state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => { dispatch(increment())},
+    decrement: () => { dispatch(decrement())}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import reducer from './reducers/counter'
 import App from './App'
 
 //创建store仓库
 const store = createStore(reducer)
-// store.subscribe(()=>console.log(store.getState()))
 
-const render = ()=>{
-    ReactDOM.render(
-        <App
-            onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-            onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-            value={store.getState()}
-        />,
-        document.getElementById('root')
-    )
-}
-render();
-store.subscribe(render)
-
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
