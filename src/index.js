461c640bb31652eedb0a7d044025cbd530325d46
CornerYoung <947498533@qq.com>
2020-03-09 16:32:40 +0800

    redux中间件和第三方中间件logger

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore} from 'redux'
+import { createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
+import logger from 'redux-logger'
 import rootReducer from './reducers'
 import App from './App'
 
+//中间件
+// const logger = store => next => action => {
+//     console.log('dispatch->',action)
+//     let result = next(action) //加载下一个中间件
+//     console.log('next state->',store.getState())
+//     return result
+// }
+
+// const error = store => next => action => {
+//     try{
+//         next(action)
+//     }catch(err){
+//         console.log('error: ',err)
+//     }
+// }
+
 //创建store仓库
-const store = createStore(rootReducer)
+// const store = createStore(rootReducer, {}, applyMiddleware(logger, error))
+const store = createStore(rootReducer, {}, applyMiddleware(logger))
 
 ReactDOM.render(
     <Provider store={store}>
--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -2,6 +2,7 @@ import * as actions from '../constants'
 const counter = (state = 0,action) => {
     switch (action.type){
         case actions.INCREMENT:
+            //throw new Error('error reducer')
             return state + action.num
         case actions.DECREMENT:
             return state - action.num
